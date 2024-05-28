2ba7b136a22ca22521a791ab404d3f3cdf898bca
Mikhail Sedov <Mikhail.Sedov@promwad.com>
2024-05-28 18:22:13 +0200

    <DSRT-322> Implement set/get flags for Ethernet payload. Add unit tests for Ethernet payload.

--- src/ethernet_payload.cpp
+++ src/ethernet_payload.cpp
@@ -7,9 +7,22 @@ uint16_t EthernetPayload::Header::getFlags() const
     return swapEndian(flags);
 }
 
-void EthernetPayload::Header::setFlags(uint16_t newFlags)
+void EthernetPayload::Header::setFlags(const uint16_t newFlags)
 {
     flags = swapEndian(newFlags);
+}
+
+bool EthernetPayload::Header::getFlag(const Flags mask) const
+{
+    return (getFlags() & static_cast<uint16_t>(mask)) != 0;
+}
+
+void EthernetPayload::Header::setFlag(const Flags mask, const bool value)
+{
+    if (value)
+        setFlags(getFlags() | static_cast<uint16_t>(mask));
+    else
+        setFlags(getFlags() & ~static_cast<uint16_t>(mask));
 }
 
 uint16_t EthernetPayload::Header::getDataLength() const
@@ -45,15 +58,34 @@ bool EthernetPayload::addSegment(const uint8_t* data, const size_t size)
     payloadData.resize(oldPayloadSize + newDataSize);
     memcpy(payloadData.data() + oldPayloadSize, data + sizeof(Header), newDataSize);
 
-    auto payloadHeader = reinterpret_cast<Header*>(payloadData.data());
-    payloadHeader->increaseDataLength(newDataSize);
+    getHeader()->increaseDataLength(newDataSize);
 
     return true;
 }
 
+uint16_t EthernetPayload::getFlags() const
+{
+    return getHeader()->getFlags();
+}
+
+void EthernetPayload::setFlags(uint16_t newFlags)
+{
+    getHeader()->setFlags(newFlags);
+}
+
+bool EthernetPayload::getFlag(Flags mask) const
+{
+    return getHeader()->getFlag(mask);
+}
+
+void EthernetPayload::setFlag(Flags mask, bool value)
+{
+    getHeader()->setFlag(mask, value);
+}
+
 uint16_t EthernetPayload::getDataLength() const
 {
-    return reinterpret_cast<const Header*>(payloadData.data())->getDataLength();
+    return getHeader()->getDataLength();
 }
 
 const uint8_t* EthernetPayload::getData() const
@@ -65,6 +97,16 @@ bool EthernetPayload::isValidPayload(const uint8_t* data, const size_t size)
 {
     auto header = reinterpret_cast<const Header*>(data);
     return (size >= sizeof(Header) && (header->getFlags() & errorMask) == 0 && header->getDataLength() <= size - sizeof(Header));
+}
+
+const EthernetPayload::Header* EthernetPayload::getHeader() const
+{
+    return reinterpret_cast<const Header*>(payloadData.data());
+}
+
+EthernetPayload::Header* EthernetPayload::getHeader()
+{
+    return reinterpret_cast<Header*>(payloadData.data());
 }
 
 END_NAMESPACE_ASAM_CMP
